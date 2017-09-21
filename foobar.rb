7bcf73d924dd6a7c856f90ece6908f6a615aa758
Sean Chen <sean@airbears2-10-142-60-157.airbears2.1918.berkeley.edu>
2017-09-20 19:53:32 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,25 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! do |s|
+      s = s.to_i
+      s += 2
+    end
+    
+    b = []
+    a.each do |x|
+      if (x%2 == 0) and (x < 10)
+	b += [x]
+      end
+    end
+
+
+    b = b.uniq
+    sum = 0
+
+    b.each do |z|
+      sum = sum + z
+    end
+    return sum
   end
 end
-
-
